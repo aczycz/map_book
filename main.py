911aba857f213dddcac59a9139596d01246ca193
aczycz <166690477+aczycz@users.noreply.github.com>
2024-04-18 09:27:15 +0200

    wip: add a menu

--- main.py
+++ main.py
@@ -11,4 +11,17 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twoj znajomy {user["name"]} opublikowal: {user["posts"]}")
 
-show_users(users)
+if __name__== "__main__":
+    print("Witaj uzytkowniku")
+
+    while True:
+
+        print("Menu")
+        print("1. Wyswietl co u znajomych")
+        menu_option:str=input("Dokonaj wyboru:")
+        if menu_option=="0":
+            print("Program konczy prace")
+            break
+            show_users(users)
+        if menu_option=="1":
+            show_users(users)
