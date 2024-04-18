fe7924780a21916bcc5ff96d50c013d87878a55a
aczycz <166690477+aczycz@users.noreply.github.com>
2024-04-18 11:20:27 +0200

    wip: add remove user function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data import users
-from utils.crud import show_users, add_new_user
+from utils.crud import show_users,add_new_user,search_user,remove_user
 
 if __name__== "__main__":
     print("Witaj uzytkowniku")
@@ -10,13 +10,20 @@ if __name__== "__main__":
         print("0. Zakoncz program")
         print("1. Wyswietl co u znajomych")
         print("2. Dodaj uzytkownika")
+        print("3. Znajdz uzytkownika")
+        print("4. Usun uzytkownika")
         menu_option:str=input("Dokonaj wyboru:")
         if menu_option=="0":
             print("Program konczy prace")
             break
             show_users(users)
-        if menu_option=="1":
+        if menu_option == "1":
             show_users(users)
-            if menu_option == "2":
-               add_new_user(users)
+        if menu_option == "2":
+            add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_user(users)
+
 
